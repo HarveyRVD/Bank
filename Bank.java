006ee0eada708d45085f07b8365d1ed291642ac1
HarveyRVD <harveyrvd@live.com>
2020-10-26 16:34:52 +0100

    first commit, error remainig in the last if statement

---
+++ Bank.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Bank {
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter the account number : ");
+        int accountNb = input.nextInt();
+
+        System.out.println("Please enter the initial balance : ");
+        int balance = input.nextInt();
+
+        System.out.println("Please enter the account pin : ");
+        int pin = input.nextInt(); //must be 1234
+        if(pin != 1234) {
+            do {
+                System.out.println("Error ! Please, enter PIN number again : ");
+                pin = input.nextInt();
+            }
+            while (pin != 1234);
+        }
+
+        System.out.println("Amount to be deposited : ");
+        double depositedAmount = input.nextDouble();
+        double newBalance = balance + depositedAmount;
+        System.out.println("Amount deposited. The new balance is : " + newBalance);
+
+        System.out.println("Amount to be withdrawn : ");
+        double withdrawnAmount = input.nextDouble();
+        if(withdrawnAmount > newBalance){
+            do{
+                System.out.println("You're not that rich ! Enter a correct amount : ");
+                withdrawnAmount = input.nextDouble();
+            }
+            while(withdrawnAmount > newBalance);
+        }
+        else {
+            System.out.println(withdrawnAmount + "$ was withdrawn from the account.");
+            newBalance = newBalance - withdrawnAmount;
+            System.out.println("The new balance is : " + newBalance);
+        }
+
+
+    }
+}
